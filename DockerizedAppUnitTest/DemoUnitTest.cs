a58f6f06e55582a51665a5b848132436872d26fc
ranjeethmd <ranjeeth.devaiah@outlook.com>
2021-06-12 23:35:45 -0700

    Updated with test cases
    Added the failed test case to see failing condition during build

---
+++ DockerizedAppUnitTest/DemoUnitTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DockerizedAppUnitTest
+{
+    [TestClass]
+    public class DemoUnitTest
+    {
+        [TestMethod]
+        public void SuccessTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void FailedTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
